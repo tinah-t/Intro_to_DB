82e4dfb901fe4427e0e73350eb2b376e23fa1c22
Hana <gerenett@gmail.com>
2025-10-19 21:57:29 +0300

    creating tables insid alx book store

---
+++ task_2.sql
@@ -0,0 +1,29 @@
+USE alx_book_store;
+CREATE TABLE Books (
+    bookid INT PRIMARY KEY, 
+    title VARCHAR(130), 
+    author_id INT FOREIGN KEY REFERENCES Authors, 
+    price DOUBLE, 
+    publication_date DATE);
+Authors: Stores information about authors.
+CREATE TABLE Authors(
+    author_id INT PRIMARY KEY, 
+    author_name VARCHAR(215), 
+);
+CREATE TABLE Customers(
+    customer_id INT PRIMARY KEY, 
+    customer_name VARCHAR(215), 
+    email VARCHAR(215), 
+    address TEXT
+);
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY, 
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), 
+    order_date DATE
+);
+CREATE TABLE Order_Details(
+    orderdetailid INT PRIMARY KEY, 
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id), 
+    book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id), 
+    quantity DOUBLE
+);
